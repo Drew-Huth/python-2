3c6bf7872edfa5d10606d734aa5e33ec222b7204
Drew-Huth <147175635+Drew-Huth@users.noreply.github.com>
2023-11-15 15:38:46 +0000

    Assignment Upload

--- src/assignments.py
+++ src/assignments.py
@@ -1,2 +1,142 @@
-# define your methods here.
-# ex1() - ex10()
+from src.WordCounter import WordCounter
+from src.TaxMan import TaxMan
+from src.Calculator import Calculator
+from src.CarCollector import CarCollector
+from pprint import pprint
+from src.Fighter import Fighter
+from src.Character import Character
+from src.Dwarf import Dwarf
+from src.Invoice import Invoice
+
+
+
+# define and/or call your methods here:
+def ex1():
+    people_list = [
+    {'name': 'alice',   'age': 20, 'weight': 160, 'sex': 'male',   'id': 1},
+    {'name': 'bob',     'age': 10, 'weight': 130, 'sex': 'male',   'id': 2},
+    {'name': 'charlie', 'age': 15, 'weight': 120, 'sex': 'female', 'id': 3},
+    ]
+    sort_people(people_list, 'weight', 'asc')
+    print(people_list)
+   
+def sort_people(people, x, y):
+    z = False    
+    if y == 'desc':
+        z = True
+    else:
+        z = False      
+    return people.sort(key=lambda p: p[x], reverse=z)
+
+def ex2():
+    people_list = [
+        {'name': 'alice',   'age': 20, 'weight': 160, 'sex': 'male',   'id': 1},
+        {'name': 'bob',     'age': 10, 'weight': 130, 'sex': 'male',   'id': 2},
+        {'name': 'charlie', 'age': 15, 'weight': 120, 'sex': 'female', 'id': 3},
+    ]
+    filtered_list = filter_males(people_list)
+    print(filtered_list)
+
+def filter_males(people_list):
+
+    new_list = list(filter(lambda p: p['sex']=='male', people_list))
+    return new_list
+
+def ex3():
+    people_list = [
+        {'id': 2, 'name': 'bob',     'weight_kg': 90, 'height_meters': 1.7},
+        {'id': 3, 'name': 'charlie', 'weight_kg': 80, 'height_meters': 1.8},
+    ]
+    new_people_list = calc_bmi(people_list)
+    print(new_people_list)
+def transform_person(person):
+    retval = {
+        'id': person['id'], 
+        'name': person['name'], 
+        'weight_kg': person['weight_kg'], 
+        'height_meters': person['height_meters'], 
+        'bmi': (round(person['weight_kg'] / person['height_meters'] ** 2, 1))
+    } 
+    return retval
+def calc_bmi(people_list):
+    return list(map(transform_person, people_list)) 
+
+def ex4():
+    people_list = [
+    {'name': 'alice',   'age': 20, 'weight': 160, 'sex': 'male',   'id': 1},
+    {'name': 'bob',     'age': 10, 'weight': 130, 'sex': 'male',   'id': 2},
+    {'name': 'charlie', 'age': 15, 'weight': 120, 'sex': 'female', 'id': 3},
+    ]
+
+    new_list = get_people(people_list)
+    print(new_list)
+
+def get_people(peoples):
+    new_list = [p['name'] for p in peoples if p['age'] >= 15]
+    return new_list
+
+def ex5():
+    sentence = "This is a test of the emergency broadcast system"
+    word_counter = WordCounter(sentence)
+    word_counter.count_words()
+    print(word_counter.get_word_count())    # Returns the number of all the words.
+    print(word_counter.get_shortest_word()) # Returns the length of the shortest word.
+    print(word_counter.get_longest_word())  # Returns the length of the longest word.
+
+def ex6():
+    items = [
+        {"id": 1, "desc": "clock", "price": 1.00},
+        {"id": 2, "desc": "socks", "price": 2.00},
+        {"id": 3, "desc": "razor", "price": 3.00},
+    ]
+    tm = TaxMan(items, "10%")
+    tm.calc_total()
+    print(tm.get_total())
+    
+def ex7():
+    calculator1 = Calculator(4, 3)
+    calculator1.add()
+    print(calculator1.get_result())
+
+    calculator2 = Calculator(4, 3)
+    calculator2.sub()
+    print(calculator2.get_result())
+
+    calculator3 = Calculator(2, 3)
+    calculator3.mul()
+    print(calculator3.get_result())
+
+    calculator4 = Calculator(8, 2)
+    calculator4.div()
+    print(calculator4.get_result())
+
+def ex8():
+    pprint(CarCollector.get_data())
+
+def ex9():
+    f = Fighter(18)
+    d = Dwarf(15)
+    print(f)
+    print(d)
+    f.fight(d)
+    d.fight(f)
+    print(f)
+    print(d)
+    
+def ex10():
+    data = [
+        "1, 2322, 10.00, False",
+        "2, 5435, 60.30, True",
+        "3, 3433, 15.63, False",
+        "4, 8439, 12.77, False",
+        "5, 3424, 11.34, False",
+    ]
+    
+    invoice_list=[]
+
+    for item in data:
+        values = item.split(", ")
+        invoice = Invoice(values[0],values[1],values[2],values[3])
+        invoice_list.append(invoice)
+        
+    pprint(invoice_list)
